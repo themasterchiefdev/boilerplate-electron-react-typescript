dfb9f393a177d6cebb2236db37a01cc118626cb5
yvrkarthik <yvrkarthik@gmail.com>
2018-06-25 10:51:16 +1200

    Created webpack, initialised typescript

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import React, { Component } from "react";
+
+const Index = () => {
+	return <div>Hello React!</div>;
+};
+
+export default Index;
+
+ReactDOM.render(<Index />, document.getElementById("index"));
